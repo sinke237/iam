772754315f291ffcba49bf1d54dff537bb41802a
sinke237 <enowsinke@gmail.com>
2024-06-28 17:00:21 +0100

    blocker: 401 unauthorised

--- keycloak-jsssss/src/components/keycloak.js
+++ keycloak-jsssss/src/components/keycloak.js
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js';
 const keycloakConfig = {
   url: 'http://192.168.64.6:8081/admin',
   realm: 'master',
-  clientId: 'oauth-client',
+  clientId: 'oauth2-client',
 };
 
 const keycloak = new Keycloak(keycloakConfig);
--- oauth2/src/main/java/org/sinke/oauth2/config/SecurityConfig.java
+++ oauth2/src/main/java/org/sinke/oauth2/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
@@ -14,7 +12,6 @@ import org.springframework.security.oauth2.client.web.server.WebSessionServerOAu
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
-
 import java.net.URI;
 
 @Configuration
@@ -24,28 +21,19 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-            .authorizeExchange(exchanges ->
-                exchanges
-                    .pathMatchers("/", "/home").permitAll()
-                    .pathMatchers("/sinke-resources").authenticated()
-                    .anyExchange().authenticated()
-            )
-            .oauth2Login(oauth2Login ->
-                oauth2Login
-                    .authenticationSuccessHandler((webFilterExchange, authentication) ->
-                        Mono.fromRunnable(() ->
-                            webFilterExchange.getExchange().getResponse()
-                                .getHeaders().setLocation(URI.create("/sinke-resources"))
-                        ).then()
-                    )
-                    .authenticationFailureHandler((webFilterExchange, exception) ->
-                        Mono.fromRunnable(() ->
-                            webFilterExchange.getExchange().getResponse()
-                                .getHeaders().setLocation(URI.create("/failed-auth"))
-                        ).then()
-                    )
-            );
-            
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/", "/home").permitAll()
+                        .pathMatchers("/sinke-resources").authenticated()
+                        .anyExchange().authenticated())
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .authenticationSuccessHandler((webFilterExchange,
+                                authentication) -> Mono.fromRunnable(() -> webFilterExchange.getExchange().getResponse()
+                                        .getHeaders().setLocation(URI.create("/sinke-resources"))).then())
+                        .authenticationFailureHandler((webFilterExchange, exception) -> Mono
+                                .fromRunnable(() -> webFilterExchange.getExchange().getResponse()
+                                        .getHeaders().setLocation(URI.create("/failed-auth")))
+                                .then()));
+
         return http.build();
     }
 
@@ -53,17 +41,17 @@ public class SecurityConfig {
     public ReactiveClientRegistrationRepository reactiveClientRegistrationRepository() {
         // Configure Keycloak Client Registration
         ClientRegistration registration = ClientRegistration.withRegistrationId("keycloak")
-            .clientId("${KEYCLOAK_CLIENT_ID}")
-            .clientSecret("${KEYCLOAK_CLIENT_SECRET}")
-            .clientName("Keycloak")
-            .scope("openid", "profile", "email")
-            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-            .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-            .authorizationUri("${KEYCLOAK_AUTH_SERVER_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/auth")
-            .tokenUri("${KEYCLOAK_AUTH_SERVER_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token")
-            .userInfoUri("${KEYCLOAK_AUTH_SERVER_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/userinfo")
-            .jwkSetUri("${KEYCLOAK_AUTH_SERVER_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/certs")
-            .build();
+                .clientId("${KEYCLOAK_CLIENT_ID}")
+                .clientSecret("${KEYCLOAK_CLIENT_SECRET}")
+                .clientName("Keycloak")
+                .scope("openid", "profile", "email")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://192.168.64.6:8081/realms/master/broker/oidc/endpoint")
+                .authorizationUri("${KEYCLOAK_AUTH_SERVER_URL}")
+                .tokenUri("${KEYCLOAK_AUTH_SERVER_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token")
+                .userInfoUri("${KEYCLOAK_AUTH_SERVER_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/userinfo")
+                .jwkSetUri("${KEYCLOAK_AUTH_SERVER_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/certs")
+                .build();
 
         return new InMemoryReactiveClientRegistrationRepository(registration);
     }
@@ -71,10 +59,5 @@ public class SecurityConfig {
     @Bean
     public ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
         return new WebSessionServerOAuth2AuthorizedClientRepository();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
